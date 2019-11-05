a8d9b0a8af5364cac20c6b6a0f0ea7cce550b3f2
ziraael <trim.maloku@hotmail.com>
2019-11-05 18:59:19 +0100

    changes?

--- public/js/app.js
+++ public/js/app.js
@@ -49511,8 +49511,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Admin\socialTrim\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Admin\socialTrim\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\socialTrim\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\socialTrim\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
