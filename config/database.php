00dde3c00f3722bc8e1855f90e9871f4fab18900
ziraael <trim.maloku@hotmail.com>
2019-11-10 20:10:29 +0100

    dbdb

--- config/database.php
+++ config/database.php
@@ -3,11 +3,12 @@
 use Illuminate\Support\Str;
 
 
-// $url = parse_url(getenv("DATABASE_URL"));
-// $host = $url["host"]??null;
-// $username = $url["user"]??null;
-// $password = $url["pass"]??null;
-// $database = substr($url["path"],1)??null;
+$url = parse_url(getenv("DATABASE_URL"));
+$host = $url["host"]??null;
+$username = $url["user"]??null;
+$password = $url["pass"]??null;
+$database = substr($url["path"],1)??null;
+
 return [
 
     /*
@@ -21,7 +22,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql_production'),
 
     /*
     |--------------------------------------------------------------------------
@@ -84,6 +85,19 @@ return [
             'sslmode' => 'prefer',
         ],
 
+        'pgsql_production' => [
+            'driver' => 'pgsql',
+            'host' => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
+            'charset' => 'utf8',
+            'prefix' => '',
+            // 'prefix_indexes' => true,
+            'schema' => 'public',
+            // 'sslmode' => 'prefer',
+        ],
+        
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DATABASE_URL'),
