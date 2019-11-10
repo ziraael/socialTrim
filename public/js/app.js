f925e11497c295d3fbf2195367d255ad27e538db
ziraael <trim.maloku@hotmail.com>
2019-11-10 19:55:56 +0100

    changesss

--- public/js/app.js
+++ public/js/app.js
@@ -49517,8 +49517,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Sindroma Fortifort\Desktop\socialTrim\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Sindroma Fortifort\Desktop\socialTrim\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\socialTrim\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\socialTrim\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
